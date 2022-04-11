c25f1202839a5476b8542435bd7f381018cc7857
Damian <damiangrosu3@gmail.com>
2022-04-11 20:42:45 +0300

    Visitor Class updated

--- src/main/java/antlr/Visitor.java
+++ src/main/java/antlr/Visitor.java
@@ -14,58 +14,8 @@ public class Visitor<T> extends GeneticsGrammarBaseVisitor<T>{
     }
 
     @Override
-    public T visitProgram(GeneticsGrammarParser.ProgramContext ctx) {
-        return super.visitProgram(ctx);
-    }
-
-    @Override
-    public T visitStatements(GeneticsGrammarParser.StatementsContext ctx) {
-        return super.visitStatements(ctx);
-    }
-
-    @Override
-    public T visitId(GeneticsGrammarParser.IdContext ctx) {
-        return super.visitId(ctx);
-    }
-
-    @Override
     public T visitDeclaration(GeneticsGrammarParser.DeclarationContext ctx) {
         return super.visitDeclaration(ctx);
-    }
-
-    @Override
-    public T visitType(GeneticsGrammarParser.TypeContext ctx) {
-        return super.visitType(ctx);
-    }
-
-    @Override
-    public T visitBool(GeneticsGrammarParser.BoolContext ctx) {
-        return super.visitBool(ctx);
-    }
-
-    @Override
-    public T visitString(GeneticsGrammarParser.StringContext ctx) {
-        return super.visitString(ctx);
-    }
-
-    @Override
-    public T visitNumber(GeneticsGrammarParser.NumberContext ctx) {
-        return super.visitNumber(ctx);
-    }
-
-    @Override
-    public T visitAlpha(GeneticsGrammarParser.AlphaContext ctx) {
-        return super.visitAlpha(ctx);
-    }
-
-    @Override
-    public T visitAlpanum(GeneticsGrammarParser.AlpanumContext ctx) {
-        return super.visitAlpanum(ctx);
-    }
-
-    @Override
-    public T visitOperator(GeneticsGrammarParser.OperatorContext ctx) {
-        return super.visitOperator(ctx);
     }
 
     @Override
@@ -79,23 +29,8 @@ public class Visitor<T> extends GeneticsGrammarBaseVisitor<T>{
     }
 
     @Override
-    public T visitField(GeneticsGrammarParser.FieldContext ctx) {
-        return super.visitField(ctx);
-    }
-
-    @Override
     public T visitComputations(GeneticsGrammarParser.ComputationsContext ctx) {
         return super.visitComputations(ctx);
-    }
-
-    @Override
-    public T visitFlow_structure(GeneticsGrammarParser.Flow_structureContext ctx) {
-        return super.visitFlow_structure(ctx);
-    }
-
-    @Override
-    public T visitCondition(GeneticsGrammarParser.ConditionContext ctx) {
-        return super.visitCondition(ctx);
     }
 
     @Override
